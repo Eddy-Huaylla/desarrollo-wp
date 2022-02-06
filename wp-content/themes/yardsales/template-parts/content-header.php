1aa3279bb39025e316f7c375f6c2f77aebaf5e10
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-02-06 11:18:45 -0500

    Enlace login o email

--- wp-content/themes/yardsales/template-parts/content-header.php
+++ wp-content/themes/yardsales/template-parts/content-header.php
@@ -9,16 +9,16 @@
 			<div class="enbezado__logo col-5 col-md-2 px-2 px-md-4">
 				<?php the_custom_logo(); ?>
 			</div>
-			<div class="encabezado__menu col-6 col-md-5">
+			<div class="encabezado__menu col-6 col-md-4">
 				<?php wp_nav_menu(
 					array(
 						"menu" => 'menu-principal'
 					)
 				); ?>
 			</div>
-			<div class="encabezado__cart col-5 col-md-1">
-				<a href="sign_in.html" class="encabezado_sign-in">
-					Sign in
+			<div class="encabezado__cart col-5 col-md-2">
+				<a href="<?= home_url("sign-in"); ?>" class="encabezado_sign-in">
+					<?= do_action("plz_signin"); ?>
 				</a>
 				<a href="#" class="encabezado__link active">
 					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/header-empty-card.svg" alt="cart">
