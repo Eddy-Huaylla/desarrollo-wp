f6a0e5479ade26134cd5f7a911e4b69c90ed5c5a
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-02-06 10:47:28 -0500

    Listando nuestros productos

--- wp-content/themes/yardsales/index.php
+++ wp-content/themes/yardsales/index.php
@@ -8,127 +8,40 @@ if (have_posts()) {
 }
 ?>
 
+<?php
+
+	$args = array(
+		"post_type" => array("producto"),
+		"posts_per_page" => -1
+	);
+
+	$productos = new WP_Query($args);
+?>
+
 <div class="productos__container">
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a class="producto__link" href="#">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
+
+	<?php
+	if( $productos->have_posts() ) {
+		while( $productos->have_posts() ) {
+			$productos->the_post();
+		?>
+
+		<div class="productos__card">
+			<?= the_post_thumbnail("post-thumbnail"); ?>
+			<div class="producto__caption">
+				<div class="productos__desc">
+					<a class="producto__link" href="<?= the_permalink(); ?>">
+						<h4 class="productos__titulo"><?= the_title(); ?></h4>
+					</a>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a class="producto__link" href="#">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a class="producto__link" href="#">
-					<h4 class="productos__titulo">Round Shelf</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a href="">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a class="producto__link" href="#">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a href="">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a class="producto__link" href="#">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a href="">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a class="producto__link" href="#">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="productos__card">
-		<figure>
-			<img src="<?= get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-		</figure>
-		<div class="producto__caption">
-			<div class="productos__desc">
-				<a href="">
-					<h4 class="productos__titulo">Retro refrigerato</h4>
-				</a>
-			</div>
-		</div>
-	</div>
+
+		<?php
+		}
+	}
+	?>
+
 </div>
 
 <?php get_footer() ?>
